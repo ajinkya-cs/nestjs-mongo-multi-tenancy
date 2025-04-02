a6caa7aa392f57261fc39325e39a3b22801ef1d1
ajinkya-cs <ajinkya23muley@gmail.com>
2025-04-03 00:19:29 +0530

    feat: "Added NPM details"

--- src/tenant.service.ts
+++ src/tenant.service.ts
@@ -1,22 +1,22 @@
 import { Injectable, Scope } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Connection, Model } from 'mongoose';
-import { Partner } from './partner.schema';
 
 @Injectable({ scope: Scope.REQUEST })
 export class TenantService {
-    private tenantModels: Record<string, Model<any>> = {};
+    private models = new Map<string, Model<any>>();
 
-    constructor(@InjectConnection() private readonly connection: Connection) {}
+    constructor(
+        @InjectConnection() private readonly connection: Connection) {}
 
-    async getModel<T>(tenantId: string, schema: any, collectionPrefix: string): Promise<Model<T>> {
-        if (!this.tenantModels[tenantId]) {
-            const partner = await this.connection.model<Partner>('Partner').findOne({ uuid: tenantId }).exec();
-            if (!partner) throw new Error('Invalid tenant');
+    getModel<T>(schema: any, prefix: string, tenantId: string): Model<T> {
+        const modelName = `${prefix}_${tenantId}`;
 
-            const collectionName = `${collectionPrefix}_${partner.name}`;
-            this.tenantModels[tenantId] = this.connection.model(collectionName, schema);
+        if (!this.models.has(modelName)) {
+            const newModel = this.connection.model<T>(modelName, schema);
+            this.models.set(modelName, newModel);
         }
-        return this.tenantModels[tenantId] as Model<T>;
+
+        return this.models.get(modelName) as Model<T>;
     }
 }
